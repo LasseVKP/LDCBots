724b6cf150d189f0847cbeb590bf503ec3f6d6e2
LasseVKP <Lassevkp@gmail.com>
2024-02-04 20:34:49 +0100

    Add blackjack command

--- economy.py
+++ economy.py
@@ -1,28 +1,81 @@
 import discord
-from vkp import BasicBot, EconomyDatabaseHandler, get_env_var
+from vkp import BasicBot, EconomyDatabaseHandler, get_env_var, floor, Blackjack
 
 
+# Create database handler
 EDB = EconomyDatabaseHandler()
 
-intents = discord.Intents().default()
-intents.message_content = True
+# Create a basic bot
+bot = BasicBot(debug_guilds=[769312138207559680])
 
-bot = BasicBot(intents=intents, debug_guilds=[769312138207559680])
+# Initialize blackjack
+blackjack_object = Blackjack()
 
 
+# Pay user, command
 @bot.slash_command()
-async def hello(ctx: discord.ApplicationContext):
-    EDB.add_balance(ctx.author, 1.5)
-    await ctx.respond("Added 5 money")
+async def pay(ctx: discord.ApplicationContext, user: discord.User, amount: float):
+
+    # Check if user is a member of the guild
+    if not ctx.guild.get_member(user.id):
+        await ctx.respond("Specified member doesn't exist in this discord server", ephemeral=True)
+        return
+
+    # Check if user receiving money is a bot
+    if user.bot:
+        await ctx.respond("You cannot pay a bot", ephemeral=True)
+        return
+
+    # Check if amount is more than 0
+    amount = floor(amount, 2)
+    if amount <= 0:
+        await ctx.respond("You have to pay more than 0", ephemeral=True)
+        return
+
+    # Check if user has enough money
+    if amount < EDB.get_balance(ctx.author):
+        await ctx.respond("Insufficient funds", ephemeral=True)
+        return
+
+    # Transfer money
+    EDB.add_balance(ctx.author, -amount)
+    EDB.add_balance(user, amount)
+
+    await ctx.respond(f"Paid {user.display_name}, {amount}")
 
 
+# Check user balance, command
 @bot.slash_command()
 async def balance(ctx: discord.ApplicationContext, user: discord.User = None):
+    # Check if a user is specified, else get author balance
     user_balance = EDB.get_balance(user) if user else EDB.get_balance(ctx.author)
 
+    # Send an int if the user doesn't have a number with decimal places
     user_balance = user_balance if user_balance != int(user_balance) else int(user_balance)
 
     await ctx.respond(user_balance)
+
+
+@bot.slash_command()
+async def blackjack(ctx: discord.ApplicationContext, amount: float):
+    amount = floor(amount, 2)
+
+    #  Make sure user can't bet less than 0
+    if amount < 0:
+        await ctx.respond("You have to bet more than 0", ephemeral=True)
+        return
+
+    # Make sure user has enough money
+    if EDB.get_balance(ctx.author) < amount:
+        await ctx.respond("Insufficient funds", ephemeral=True)
+        return
+
+    # Create a view and embed and send it
+    blackjack_view = blackjack_object.create_view(ctx.author, amount, EDB)
+    await ctx.respond(embed=blackjack_view.embed, view=blackjack_view)
+
+    # Remove amount from user balance to make sure they can't open multiple blackjacks with non-existent money
+    EDB.add_balance(ctx.author, -amount)
 
 
 bot.run(get_env_var("ECONOMY_TOKEN"))
--- vkp.py
+++ vkp.py
@@ -1,14 +1,17 @@
-import discord, pymongo, os
+import discord, pymongo, os, json, random
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
 
+# Bot classes
 class BasicBot(discord.Bot):
     async def on_ready(self):
         print(f"Logged in as {self.user}")
 
 
+# Database classes
 class BaseDatabaseHandler:
     def __init__(self):
         db_client = pymongo.MongoClient(get_env_var("DATABASE_URL"))
@@ -32,15 +35,235 @@ class EconomyDatabaseHandler(BaseDatabaseHandler):
         else:
             return 0.0
 
-    # Add balance to user
+    # Add to user balance
     def add_balance(self, user: discord.User, amount: float):
         user_properties = self.money_col.find_one({"_id": user.id}) or False
+
         # Check if user is already created
         if user_properties:
-            self.money_col.find_one_and_update({"_id": user.id}, {"$set": {"balance": round(user_properties["balance"]+amount, 2)}})
+            self.money_col.find_one_and_update({"_id": user.id},
+                                               {"$set": {"balance": floor(user_properties["balance"]+amount, 2)}})
+            return floor(user_properties["balance"]+amount, 2)
         else:
-            self.money_col.insert_one({"_id": user.id, "balance": round(amount, 2)})
+            self.money_col.insert_one({"_id": user.id, "balance": floor(amount, 2)})
+            return floor(amount, 2)
 
 
+class Blackjack:
+    def __init__(self):
+        self.deck = None
+        self.load_deck()
+
+    def load_deck(self):
+        with open("templates/cardDeck.json", "r") as f:
+            self.deck = json.load(f)
+
+    class BlackJackView(discord.ui.View):
+        def __init__(self, deck: list, user: discord.User, amount: float, db: EconomyDatabaseHandler):
+            super().__init__()
+            self.deck = deck
+            self.user = user
+            self.amount = amount
+            self.db = db
+            self.title = "♠ Blackjack ♦"
+            self.footer = "Dealer must draw to 16 and stand on all 17's"
+            self.timestamp = datetime.now()
+            self.footer_icon = user.display_avatar.url
+            self.user_hand = [self.random_card() for _ in range(2)]
+            self.dealer_hand = [self.random_card() for _ in range(2)]
+            self.embed = self.check_for_blackjack() or self.current_hand_embed()
+            self.timeout = 120
+
+        @discord.ui.button(label="Hit", style=discord.ButtonStyle.primary)
+        async def hit_callback(self, _, interaction: discord.Interaction):
+            # Check if the user is the correct user
+            if interaction.user is not self.user:
+                await interaction.response.defer()
+                return
+
+            embed = self.user_draw()
+
+            await interaction.response.edit_message(embed=embed, view=self)
+
+        @discord.ui.button(label="Stand", style=discord.ButtonStyle.primary)
+        async def stand_callback(self, _, interaction: discord.Interaction):
+            # Check if the user is the correct user
+            if interaction.user is not self.user:
+                await interaction.response.defer()
+                return
+
+            embed = self.dealer_draw()
+
+            await interaction.response.edit_message(embed=embed, view=self)
+
+        # Check for dealer or user blackjack
+        def check_for_blackjack(self):
+            if self.calculate_hand(self.user_hand) == 21:
+                if self.calculate_hand(self.dealer_hand) == 21:  # Draw
+                    return self.game_draw()
+                return self.user_win("Blackjack!", floor(self.amount*1.5, 2))  # User Blackjack
+            if self.calculate_hand(self.dealer_hand) == 21:  # Dealer blackjack
+                return self.dealer_win("Dealer got blackjack!")
+
+        # User draws card
+        def user_draw(self):
+            self.user_hand.append(self.random_card())
+            if self.calculate_hand(self.user_hand) > 21:  # User bust
+                return self.dealer_win("Bust!")
+            if self.calculate_hand(self.user_hand) == 21:  # Time for dealer to draw
+                return self.dealer_draw()
+
+            return self.current_hand_embed()  # Nothing happens
+
+        # Dealer draws cards
+        def dealer_draw(self):
+            while self.calculate_hand(self.dealer_hand) < 17:
+                self.dealer_hand.append(self.random_card())
+
+            dealer_value = self.calculate_hand(self.dealer_hand)
+            user_value = self.calculate_hand(self.user_hand)
+
+            if dealer_value > 21:  # Dealer bust
+                return self.user_win("Dealer bust!", self.amount)
+            if dealer_value == user_value:  # Draw
+                return self.game_draw()
+            if dealer_value > user_value:  # Dealer win
+                return self.dealer_win("Dealer won!")
+
+            return self.user_win("You won!", self.amount)  # User win
+
+        def dealer_win(self, reason: str):
+            embed = self.current_hand_embed(True)
+            embed.add_field(name=reason,
+                            value=f"You lost {self.amount}")
+
+            self.disable_all_items()
+            self.stop()
+            return embed
+
+        def user_win(self, reason: str, amount: float):
+            embed = self.current_hand_embed(True)
+            embed.add_field(name=reason,
+                            value=f"You won {amount}")
+
+            self.db.add_balance(self.user, amount*2)
+
+            self.disable_all_items()
+            self.stop()
+            return embed
+
+        def game_draw(self):
+            embed = self.current_hand_embed(True)
+            embed.add_field(name="Draw!",
+                            value="You get your money back!")
+
+            self.db.add_balance(self.user, self.amount)
+
+            self.disable_all_items()
+            self.stop()
+            return embed
+
+        # Get a random card and remove it from the deck
+        def random_card(self):
+            card = random.choice(self.deck)
+            self.deck.remove(card)
+            return card
+
+        def calculate_hand(self, hand):
+            total_value = 0
+            ace_count = 0
+
+            # Add all card values, and assume ace is 11
+            for card in hand:
+                value = card["value"]
+                if value == "ace":
+                    ace_count += 1
+                    total_value += 11
+                else:
+                    total_value += value
+
+            # Adjust value of aces
+            while ace_count > 0 and total_value > 21:
+                total_value -= 10
+                ace_count -= 1
+
+            return total_value
+
+        def current_hand_embed(self, win: bool = False):
+            return simple_embed(title="♠ Blackjack ♦", fields=self.hand_embed_fields(win),
+                                footer=self.footer, timestamp=self.timestamp,
+                                footer_icon=self.footer_icon)
+
+        def hand_embed_fields(self, win: bool = False):
+
+            # Hide one card if game isn't won yet
+            dealer_hand = self.dealer_hand if win else [self.dealer_hand[0], {"card": "<:cardBack:941039219135086622>",
+                                                                              "value": 0}]
+
+            dealer_cards = "".join([card["card"] for card in dealer_hand])
+            dealer_value = self.calculate_hand(dealer_hand)
+
+            user_cards = "".join([card["card"] for card in self.user_hand])
+            user_value = self.calculate_hand(self.user_hand)
+
+            return [
+                    {"name": f"Dealer | {dealer_value}",
+                     "value": dealer_cards},
+                    {"name": f"User | {user_value}",
+                     "value": user_cards}
+                    ]
+
+        async def on_timeout(self):
+            embed = self.dealer_draw()
+            await self.message.edit(embed=embed, view=self)
+
+    def create_view(self, user: discord.User, amount: float, db: EconomyDatabaseHandler):
+        return self.BlackJackView(self.deck, user, amount, db)
+
+
+# Default values
+class Default:
+    COLOUR = 0x3044ff
+    ERROR_COLOUR = 0xFF3030
+    SUCCESS_COLOUR = 0x30FF33
+    EMBED_COLOUR = 0x202225
+    EMBED_BACKGROUND_COLOUR = 0x2f3136
+    BLACK = 0x000000
+
+
+def simple_embed(title=None, description=None, colour=Default.COLOUR, url=discord.Embed.Empty, fields=None, author_name=None,
+               author_url=discord.Embed.Empty, author_icon=discord.Embed.Empty, footer=discord.Embed.Empty, footer_icon=discord.Embed.Empty,
+               thumbnail=None, image=None, timestamp=None):  # More embed options
+    if fields is not None:
+        new_fields = fields
+        fields = []
+        for field in new_fields:
+            if "inline" not in field:
+                field["inline"] = False
+            fields.append(discord.EmbedField(field['name'], field['value'], field['inline']))
+    embed = discord.Embed(colour=colour, url=url, fields=fields, timestamp=timestamp)
+    if title:
+        embed.title = title
+    if description:
+        embed.description = description
+    if not title and not description:
+        embed.description = "_ _"
+    if author_name:
+        embed.set_author(name=author_name, icon_url=author_icon, url=author_url)
+    if thumbnail:
+        embed.set_thumbnail(url=thumbnail)
+    if image:
+        embed.set_image(url=image)
+    embed.set_footer(text=footer, icon_url=footer_icon)
+
+    return embed
+
+
+# Round to floor with n amount of decimal places
+def floor(i, n):
+    return round(int(i * 10**n) / 10**n, n)
+
+
+# Might delete later
 def get_env_var(key: str):
     return os.getenv(key)
